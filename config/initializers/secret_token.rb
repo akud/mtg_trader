72f408f5c0e8ed87fa9c400fceee09ac0a0938bd
Alex Kudlick <adkudlick@gmail.com>
2013-10-28 21:00:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MtgTrader::Application.config.secret_token = '924a90c04d5cd5bd7bd47ad4a5453a1604049b5c9847a31c94100664aece5678433ef1b2a7d14cded9a96391ce1d2bdf3d4458edb1c2779924caaf1218080af8'
